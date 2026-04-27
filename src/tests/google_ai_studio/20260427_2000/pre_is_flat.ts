6378d09fb00a86aaf31efd47ce6823c93f60a9af
norlz <norlz@gitkra.com>
2026-04-27 20:28:55 +0800

    ^

--- src/tests/google_ai_studio/20260427_2000/pre_is_flat.ts
+++ src/tests/google_ai_studio/20260427_2000/pre_is_flat.ts
@@ -1,18 +1,18 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import { fileURLToPath } from 'url';
-import { run_conversion } from '../support/tester';
-import { GoogleAIStudioAdapter } from '../../platforms/GoogleAIStudioAdapter';
+import { run_conversion } from '../../support/tester';
+import { GoogleAIStudioAdapter } from '../../../platforms/GoogleAIStudioAdapter';
 
 // Fix for __dirname in ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // const htmlPath = path.join(__dirname, 'main_test/complex_2.html');
-const htmlPath = path.join(__dirname, 'main_test/complex_3.html');
+const htmlPath = path.join(__dirname, 'pre_is_flat.html');
 const html = fs.readFileSync(htmlPath, 'utf8');
 
-import { MarkdownFormatter } from '../../core/MarkdownFormatter';
+import { MarkdownFormatter } from '../../../core/MarkdownFormatter';
 
 const adapter = new GoogleAIStudioAdapter();
 const url = 'https://aistudio.google.com/'; 
